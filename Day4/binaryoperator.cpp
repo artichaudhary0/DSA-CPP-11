5209247bfda8b44f8080caaca998c38076dc3ac1
artichaudhary0 <chaudhary.aarti1998@gmail.com>
2025-03-10 12:47:41 +0530

    rivision

--- Day4/binaryoperator.cpp
+++ Day4/binaryoperator.cpp
@@ -48,3 +48,79 @@ int main()
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Revision
+
+
+
+
+#include <iostream>
+using namespace std;
+
+/*
+    Binary operator : 
+        1 : Arithmetic operator : 
+                + , - , * , / , % 
+        2 : Conditional operator : 
+                > , < , >= , <= , == 
+        3 : Assignment operator : shorthand operator
+                = , =+ , -= , *= , /= , %=
+        4 : Logical operator : 
+                || && !
+        5 : Bitwise 
+                | & >> << 
+*/
+
+int main() {
+    // cout<<"SUM 10 + 20 : "<<10 + 20 <<endl; // 30
+    // cout<<"SUB 10 - 20 : "<<10 - 20 <<endl; // -10
+    // cout<<"MULTIPLICATION 10 * 20 : "<<10 * 20 <<endl; // 200
+    // cout<<"DIVISION 10 / 20 : "<<10.0 / 20.0 <<endl; 
+    // // int/int => int 10/20 =>int 0.5 => 0
+    // // int / float => float => 0.5
+    // // float/int => float => 0.5
+    // // float / float => float => 0.5
+    // cout<<"REMAINDER 10 % 20 : "<<21 % 20 <<endl; // 1
+    
+    //  2 : Conditional operator : 
+    //             greater then > , less then < , greaterthen or equal to>= , less then or equals to<= , equals== 
+    // 1 : true , 0 : false
+    // int result = 10 <= 110; // 10 < 110 || 10 == 110 => true || false
+    
+    // cout<< result;
+    
+    // 3 : Assignment operator : 
+    //             = , =+ , -= , *= , /= , %=
+    int result = 5466;
+    // right side ki value left side ko diya.
+    
+
+    result += 213 ; //    result = result + 213;
+    cout<<result<<endl;
+    
+    result -= 213 ; //    result = result - 213 => 5466;
+    cout<<result<<endl;
+    
+    result *= 213 ; //    result = result * 213; 
+    cout<<result<<endl; // 11,64,258
+    
+    result /= 213 ; //    result = result / 213; 11,64,258/213 => 5,466
+    cout<<result<<endl;
+    
+    result %= 213 ; //    result = result % 213; 5466 % 213
+    cout<<result<<endl;
+    return 0;
+}
